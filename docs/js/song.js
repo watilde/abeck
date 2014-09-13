a5c04d072730d80d79695b730686d3a6a47b5c65
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-09-13 14:13:09 +0900

    Update abeck

--- docs/js/song.js
+++ docs/js/song.js
@@ -2,20 +2,26 @@ var song = new abeck({
   X: 1,
   title: 'The Song',
   bpm: 60,
-  meter: '1/4',
-  length: 1,
+  meter: '4/4',
+  length: '1/4',
   rhythm: 'reel',
-  key: 'Bmaj'
+  key: 'Cmaj'
 });
 
 song.push([
-    [null, 4]
+    [null, '4']
   ])
   .push([
-    ['C', 1/4],
-    ['^C', 1/4],
-    ['D', 1/4],
-    ['D', 1/4]
+    ['C', '1'],
+    ['^C', '1'],
+    ['D', '1'],
+    ['^D', '1']
+  ])
+  .push([
+    ["C'", '1'],
+    ["D'", '1'],
+    ["E'", '1'],
+    ["F'", '1']
   ]);
 
 song.play();
